39a8b79ce9ea6d474e112f5f3de65639b63fdab3
yikewudidabaicai <1659800546@qq.com>
2019-09-10 17:28:14 +0800

    第一次作业提交

---
+++ zuoye.java
@@ -0,0 +1,17 @@
+public class zuoye{
+     public static void main (String[] args){
+     System.out.print("////*//////////////////////////////\n");
+     System.out.print("////*////****////*/////*////****///\n");
+     System.out.print("////*///*////*////*///*////*////*//\n");
+     System.out.print("////*///*////*/////*/*/////*////*//\n");
+     System.out.print("/*//*////****/*/////*///////****/*/\n");
+     System.out.print("/****//////////////////////////////\n");
+
+
+}
+}
+
+
+
+
+
